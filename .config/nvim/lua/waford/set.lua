e6a171d1ad56ebc71da7e7b0d01cbcfe69dd8a38
Wesley Ford <wford32@gatech.edu>
2022-09-10 17:15:46 -0400

    Commented set.lua and added colorcolumn to limit line width

--- .config/nvim/lua/waford/set.lua
+++ .config/nvim/lua/waford/set.lua
@@ -1,21 +1,29 @@
 vim.opt.guicursor = ""
-vim.opt.nu = true
-vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Line Numberrs
+vim.opt.number = true                   -- Enable line numbers
+vim.opt.relativenumber = true           -- Enable relative liune numbers
 
-vim.opt.smartindent = true
-vim.opt.wrap = false
+-- Indenting and Formating
+vim.opt.tabstop = 4                     -- Sets width of tabs to 4 spaces
+vim.opt.softtabstop = 4                 -- Simulate tabstops of 4 spaces
+vim.opt.shiftwidth = 4                  -- Sets width of indent to 4 spaces
+vim.opt.expandtab = true                -- Expands tab key to spaces
+vim.opt.smartindent = true              -- Tabing will indent
+vim.opt.autoindent = true               -- Enable autoindent
+vim.opt.colorcolumn = '100'              -- Place a column at 100 characters
 
-vim.opt.spelllang = en_us
-vim.opt.spell = true
+-- Search Options
+vim.opt.hlsearch = false                -- Disable search highlighting
+vim.opt.incsearch = true                -- Enable incremental search
 
-vim.g.mapleader = " "
+vim.opt.wrap = false                    -- Disable line wraping
+
+-- Spelling
+vim.opt.spelllang = en_us               -- Set spell file
+vim.opt.spell = true                    -- Enable spell check
+
+vim.g.mapleader = " "                   -- Set leader to space
 
 -- Configure coq_nvim pluigin --
 vim.g.coq_settings = { 
