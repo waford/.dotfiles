efadc45ec22be06da1ab1281e721f52a1bb3956b
Wesley Ford <wford32@gatech.edu>
2022-09-12 10:26:48 -0400

    Austosave will now compile .tex into pdf with a .bib file

--- .config/nvim/lua/waford/autosave.lua
+++ .config/nvim/lua/waford/autosave.lua
@@ -1,13 +1,25 @@
+local start_pdflatex = function(filename)
+    local pdflatex_flags = '-interaction=nonstopmode'
+    local dot_index = string.find(filename, ".tex")
+    local bibtex_file = string.sub(filename, 0, dot_index - 1)
+    local pdflatex_cmd = "pdflatex " .. pdflatex_flags .. " " .. filename .. ";"
+    local bibtex_cmd = "bibtex " .. bibtex_file .. ";"
+    local tex_compile_cmd = pdflatex_cmd .. bibtex_cmd .. pdflatex_cmd .. pdflatex_cmd
+
+    local job_id = vim.fn.jobstart(tex_compile_cmd, {
+        on_exit = function()
+            print("Done!")
+        end,
+    })
+    vim.fn.jobwait({job_id})
+end
+
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("latex", { clear = true }),
     pattern = "*.tex",
     callback = function()
         local filename = vim.fn.getreg('%')
-        local pdflatex_flags = '-interaction=nonstopmode'
-        vim.fn.jobstart({"pdflatex", pdflatex_flags, filename}, {
-            on_exit = function()
-                print("Created pdf of " .. filename)
-            end,
-        })
+        start_pdflatex(filename)
     end,
 })
+
