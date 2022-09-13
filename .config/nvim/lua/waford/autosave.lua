59662dffd1fbeb7fea267590353018f2b31e8167
Wesley Ford <wford32@gatech.edu>
2022-09-13 09:47:25 -0400

    Updated autosave and i3 config

--- .config/nvim/lua/waford/autosave.lua
+++ .config/nvim/lua/waford/autosave.lua
@@ -6,11 +6,7 @@ local start_pdflatex = function(filename)
     local bibtex_cmd = "bibtex " .. bibtex_file .. ";"
     local tex_compile_cmd = pdflatex_cmd .. bibtex_cmd .. pdflatex_cmd .. pdflatex_cmd
 
-    local job_id = vim.fn.jobstart(tex_compile_cmd, {
-        on_exit = function()
-            print("Done!")
-        end,
-    })
+    local job_id = vim.fn.jobstart(tex_compile_cmd)
     vim.fn.jobwait({job_id})
 end
 
