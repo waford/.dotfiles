e537d1ad1205c564377918f48e786b7c056df91c
Wesley Ford <webwesley@fordcochrane.com>
2023-02-21 09:12:07 -0500

    Added keybindings for tmux split sessions

--- .config/nvim/lua/waford/set.lua
+++ .config/nvim/lua/waford/set.lua
@@ -1,33 +1,33 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor=""
 
 -- Line Numberrs
-vim.opt.number = true                   -- Enable line numbers
-vim.opt.relativenumber = true           -- Enable relative liune numbers
+vim.opt.number=true                   -- Enable line numbers
+vim.opt.relativenumber=true           -- Enable relative liune numbers
 
 -- Indenting and Formating
-vim.opt.tabstop = 4                     -- Sets width of tabs to 4 spaces
-vim.opt.softtabstop = 4                 -- Simulate tabstops of 4 spaces
-vim.opt.shiftwidth = 4                  -- Sets width of indent to 4 spaces
-vim.opt.expandtab = true                -- Expands tab key to spaces
-vim.opt.smartindent = true              -- Tabing will indent
-vim.opt.autoindent = true               -- Enable autoindent
-vim.opt.colorcolumn = '80'              -- Place a column at 80 characters
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+vim.opt.tabstop=4                     -- Sets width of tabs to 4 spaces
+vim.opt.softtabstop=4                 -- Simulate tabstops of 4 spaces
+vim.opt.shiftwidth=4                  -- Sets width of indent to 4 spaces
+vim.opt.expandtab=true                -- Expands tab key to spaces
+vim.opt.smartindent=true              -- Tabing will indent
+vim.opt.autoindent=true               -- Enable autoindent
+vim.opt.colorcolumn='80'              -- Place a column at 80 characters
+vim.opt.termguicolors=true
+vim.opt.scrolloff=8
 
-vim.opt.splitright = true
+vim.opt.splitright=true
 
 -- Search Options
-vim.opt.hlsearch = false                -- Disable search highlighting
-vim.opt.incsearch = true                -- Enable incremental search
+vim.opt.hlsearch=false                -- Disable search highlighting
+vim.opt.incsearch=true                -- Enable incremental search
 
-vim.opt.wrap = false                    -- Disable line wraping
+vim.opt.wrap=false                    -- Disable line wraping
 
 -- Spelling
-vim.opt.spelllang = en_us               -- Set spell file
-vim.opt.spell = true                    -- Enable spell check
+vim.opt.spelllang=en_us               -- Set spell file
+vim.opt.spell=true                    -- Enable spell check
 
-vim.g.mapleader = " "                   -- Set leader to space
+vim.g.mapleader=" "                   -- Set leader to space
 
 
 
