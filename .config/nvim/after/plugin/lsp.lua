02a1b35dba8596e7a6438e1392c3f68e73640a42
Wesley Ford <wford32@gatech.edu>
2023-05-28 08:53:18 -0700

    Update

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,10 @@ local nnoremap = require("waford.keymap").nnoremap
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-
-
+lsp.setup_nvim_cmp({
+    preselect = 'none',
+    completion = {
+	completeopt = 'menu,menuone,noinsert,noselect'
+    },
+})
 lsp.setup()
-
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -134,6 +134,12 @@ _G.packer_plugins = {
     path = "/home/waford/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  nightfox = {
+    config = { "\27LJ\2\n8\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\25colorscheme nightfox\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/waford/.local/share/nvim/site/pack/packer/start/nightfox",
+    url = "https://github.com/EdenEast/nightfox.nvim"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/waford/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -164,12 +170,6 @@ _G.packer_plugins = {
     path = "/home/waford/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["rose-pine"] = {
-    config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
-    loaded = true,
-    path = "/home/waford/.local/share/nvim/site/pack/packer/start/rose-pine",
-    url = "https://github.com/rose-pine/neovim"
-  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/home/waford/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -188,10 +188,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
+-- Config for: nightfox
+time([[Config for nightfox]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\25colorscheme nightfox\bcmd\bvim\0", "config", "nightfox")
+time([[Config for nightfox]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
